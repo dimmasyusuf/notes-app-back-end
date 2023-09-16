3edc7dc71e6b7c76f33cdb9050227649ab612f96
Dimas Yusuf Qurohman <dimasyusufqurohman@gmail.com>
2023-09-16 16:41:05 +0700

    feat: delete note by id

--- src/handler.js
+++ src/handler.js
@@ -102,3 +102,26 @@ export const editNoteByIdHandler = (request, h) => {
   response.code(404);
   return response;
 };
+
+export const deleteNoteByIdHandler = (request, h) => {
+  const { id } = request.params;
+
+  const index = notes.findIndex((note) => note.id === id);
+
+  if (index !== -1) {
+    notes.splice(index, 1);
+    const response = h.response({
+      status: 'success',
+      message: 'Catatan berhasil dihapus',
+    });
+    response.code(200);
+    return response;
+  }
+
+  const response = h.response({
+    status: 'fail',
+    message: 'Catatan gagal dihapus. Id tidak ditemukan',
+  });
+  response.code(404);
+  return response;
+};
